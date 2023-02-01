84b89bcdace7b46c8093ce7e4ba4eb6d4a6930b4
MiguelLopezMasmela <minep623@gmail.com>
2023-02-01 13:04:36 -0500

    agregue lista de productos

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const mobileMenu_icon = document.querySelector(".menu");
 const mobilMenu = document.querySelector(".mobile-menu");
 const cartIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector('.product-detail');
-
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 mobileMenu_icon.addEventListener('click',toggleMobileMenu);
@@ -26,3 +26,37 @@ function toggleCarritoAside(){
     mobilMenu.classList.add('inactive')
     desktopMenu.classList.add('inactive');
 }
+
+let product = {name:'Bike',price:120,img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"};
+
+const productList = [];
+
+for(let i=0 ; i<10;i++){
+    productList.push(product);
+}
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+    
+        const img =  document.createElement('img');
+        img.setAttribute('src',product.img)
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info')
+        const productInfoDiv = document.createElement('div')
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$'+product.price
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+        const productInfoFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.append(productImgCart)
+        productInfoDiv.append(productPrice,productName)
+        productInfo.append(productInfoDiv,productInfoFigure)
+        productCard.append(img,productInfo)
+        cardsContainer.append(productCard)
+}
+}
+renderProducts(productList)
