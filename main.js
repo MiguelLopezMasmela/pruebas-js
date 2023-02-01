ea46f4364c850124272ab26b01a1d07441a9149a
MiguelLopezMasmela <minep623@gmail.com>
2023-02-01 09:32:25 -0500

    Fusion menu mobile (Yardsale)

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+const mobileMenu_icon = document.querySelector(".menu")
+const mobilMenu = document.querySelector(".mobile-menu")
 
 const toggleDesktopMenu = () =>{
     desktopMenu.classList.toggle('inactive')
 
 }
+
+const toggleMobileMenu = () =>{
+    mobilMenu.classList.toggle('inactive')
+
+}
 menuEmail.addEventListener('click', toggleDesktopMenu);
+mobileMenu_icon.addEventListener('click',toggleMobileMenu)
