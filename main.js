51855fa6ccf703dacdf336c6608f12020bef112f
MiguelLopezMasmela <minep623@gmail.com>
2023-02-01 10:08:56 -0500

    aside carrito compras

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu")
-const mobileMenu_icon = document.querySelector(".menu")
-const mobilMenu = document.querySelector(".mobile-menu")
+const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu_icon = document.querySelector(".menu");
+const mobilMenu = document.querySelector(".mobile-menu");
+const cartIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector('.product-detail');
 
-const toggleDesktopMenu = () =>{
-    desktopMenu.classList.toggle('inactive')
 
-}
-
-const toggleMobileMenu = () =>{
-    mobilMenu.classList.toggle('inactive')
-
-}
 menuEmail.addEventListener('click', toggleDesktopMenu);
-mobileMenu_icon.addEventListener('click',toggleMobileMenu)
+mobileMenu_icon.addEventListener('click',toggleMobileMenu);
+cartIcon.addEventListener('click',toggleCarritoAside)
+
+
+function toggleDesktopMenu () {
+    desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+function toggleMobileMenu(){
+    mobilMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+function toggleCarritoAside(){
+    aside.classList.toggle('inactive');
+    mobilMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive');
+}
